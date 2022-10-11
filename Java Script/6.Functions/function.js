66c4050c41d65fbce9896ae58b5632e9f3860194
Mohdshoaib888 <beingshoaib4@gmail.com>
2022-10-11 22:10:04 +0530

    Next syallabus

---
+++ Java Script/6.Functions/function.js
@@ -0,0 +1,35 @@
+// console.log("Testing");
+// function declaration
+
+// Declaration ();
+// function declaration () {
+//     return console.log ("Hello world")
+// };
+
+// Function expression
+// expression ();
+// var expression = function () {
+//     return console.log("Hello i am second function")
+// };
+
+// Function with arguements
+
+// function addition (val, val2) {
+//     return val + val2;
+// }
+// console.log (addition (50, 30));
+
+// rest parameter
+
+// function rest (val1, ...rest) {
+//     console.log(rest)
+// }
+// rest (1,2,3,4,5,6,7,8,9,10)
+
+// Advance function arguement
+
+function advance (value1, value2) {
+console.log(value1);
+console.log(value2);
+}
+advance([1,2,3], { name: "name", age:"20", add:"adress"});
